f4cc22f982e0e7d78627ce65fc854c0a053723b0
jasonw040409 <tongyang.w@wustl.edu>
2026-03-24 13:11:52 -0500

    Restored files. Notvalid code yet

---
+++ runApp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+cd "$(dirname "$0")"
+
+rm -rf out
+mkdir out
+
+javac -d out -cp "lib/*" src/main/*.java src/test/*.java
+java -jar lib/junit-platform-console-standalone-1.13.0-M3.jar -cp out --scan-classpath
---
+++ src/main/BankAccount.java
@@ -0,0 +1,31 @@
+package main;
+//
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount() {
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            this.balance += amount;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (amount > this.balance){
+            throw new IllegalArgumentException();
+        }
+        this.balance -= amount;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
